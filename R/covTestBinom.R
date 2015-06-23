0a1e5856b78f2b6a659712ce99d967fb134d4018
TroyHernandez <Troy.Hernandez.PhD@gmail.com>
2015-06-23 14:27:17 -0500

    Refactored output.

--- R/covTestBinom.R
+++ R/covTestBinom.R
@@ -31,10 +31,10 @@ covTestBinom <- function (x, y, weights, maxp = "num.nonzero",
   lambda.ind <- as.numeric(names(table(beta.mat.enter)))
   lamlist = c(glmobj$lambda[lambda.ind], 0)
   
-  # jlist is the active set of variables
-  jlist = vector("list", length = length(lambda.ind))#unlist(fitobj$act) # Predictors added at each step.
+  # var.list is the active set of variables
+  var.list = vector("list", length = length(lambda.ind))#unlist(fitobj$act) # Predictors added at each step.
   for(i in 1:length(lambda.ind)){
-    jlist[[i]] <- which(beta.mat.enter == lambda.ind[i])
+    var.list[[i]] <- which(beta.mat.enter == lambda.ind[i])
   }
   
   # maxp is limiting the number of things to check
@@ -43,10 +43,10 @@ covTestBinom <- function (x, y, weights, maxp = "num.nonzero",
     # num.nonzero <- sum(glmobj$beta[, 1:which.lam.min] != 0)
     maxp = which.lam.min #num.nonzero
   } else {
-    maxp = min(c(length(jlist), which(lamlist == 0), nrow(x), ncol(x)), maxp)
+    maxp = min(c(length(var.list), which(lamlist == 0), nrow(x), ncol(x)), maxp)
   }
   
-  jlist = jlist[1:maxp]
+  var.list = var.list[1:maxp]
   cov0 = cov = sig = rep(NA, maxp)
   yy = y - my
 
@@ -61,7 +61,7 @@ covTestBinom <- function (x, y, weights, maxp = "num.nonzero",
       cov0[j] = 0
     }
     if (j > 1) {
-      tt0 = unlist(jlist[1:j])
+      tt0 = unlist(var.list[1:j])
       #       This is the shortcut that does not work.  It needs to rerun path to 
       #       yhat0 = 1 / (1 + exp(x[, tt0, drop = F] %*%
       #                              glmobj$beta[tt0,lambda.ind[j]] +
@@ -75,26 +75,49 @@ covTestBinom <- function (x, y, weights, maxp = "num.nonzero",
     }
   }
   
-  # compile results
-  tt = cov - cov0
-  
-  results = cbind(tt, pexp(tt, 1, lower.tail = FALSE))
-  colnames(results) = c("Drop_in_Cov", "P-value")
+  cov.stat = cov - cov0
 
   which.lam.min <- which(glm.mod$lambda == glm.mod$lambda.min)
   which.lam.1se <- which(glm.mod$lambda == glm.mod$lambda.1se)
 
+  # compile results
+  var.list.pvalue = cbind(cov.stat = cov.stat,
+                          p.value = pexp(cov.stat, 1, lower.tail = FALSE),
+                          beta.min = rep(NA, length(cov.stat)),
+                          beta.1se = rep(NA, length(cov.stat)),
+                          log.min = rep(NA, length(cov.stat)),
+                          log.1se = rep(NA, length(cov.stat)))
+  # Set rownames so we can rewrite them later.
+  rownames(var.list.pvalue) <- as.character(c(1:length(var.list)))
+  
+  for(i in 1:length(var.list)){
+    ind <- var.list[[i]]
+    # if(length(ind) > 1){}
+    rownames(var.list.pvalue)[i] <- paste(row.names(glmobj$beta)[ind],
+                                          collapse = "|")
+    var.list.pvalue[i, "beta.min"] <- mean(glmobj$beta[ind, which.lam.min])
+    var.list.pvalue[i, "beta.1se"] <- mean(glmobj$beta[ind, which.lam.1se])
+    var.list.pvalue[i, "log.min"] <- mean(exp(glmobj$beta[ind, which.lam.min]))
+    var.list.pvalue[i, "log.1se"] <- mean(exp(glmobj$beta[ind, which.lam.1se]))
+  }
+
   var.pvalue <- data.frame(cov.stat = rep(NA, dim(x)[2]),
                            p.value = rep(NA, dim(x)[2]),
                            beta.min = glmobj$beta[, which.lam.min],
-                           beta.1se = glmobj$beta[, which.lam.1se])
-  for(i in 1:length(jlist)){
-    for(j in 1:length(jlist[i][[1]])){
-      var.pvalue[jlist[i][[1]][j], "cov.stat"] <- results[i, 1]
-      var.pvalue[jlist[i][[1]][j], "p.value"] <- results[i, 2]
+                           beta.1se = glmobj$beta[, which.lam.1se],
+                           log.min = exp(glmobj$beta[, which.lam.min]),
+                           log.1se = exp(glmobj$beta[, which.lam.1se]))
+
+  for(i in 1:length(var.list)){
+    indlen <- length(var.list[[i]])
+    for(j in 1:length(var.list[[i]])){
+      temp.cov.stat <- var.list.pvalue[i, 1] / indlen
+      var.pvalue[var.list[[i]][j], "cov.stat"] <- temp.cov.stat
+      var.pvalue[var.list[[i]][j], "p.value"] <- pexp(temp.cov.stat, 1,
+                                                      lower.tail = FALSE)
     }
   }
-  
-  list(results = results, var.list = jlist,
+
+  list(var.list.pvalue = var.list.pvalue, var.list = var.list,
        var.pvalue = var.pvalue, glm.mod = glm.mod)
 }
